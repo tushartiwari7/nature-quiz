d722f557ee184078bca4adb3c0e2ba45cb47e902
Tushar Tiwari <tushartiwari0172@gmail.com>
2022-02-15 00:32:17 +0530

    feat: included all quizz types/categories

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const continueBtn = document.getElementById('btn-continue');
 
 continueBtn.addEventListener('click',()=>{
-  console.log("clicked");
   document.getElementById('modal').style.display = 'none';
   document.getElementById('main').style.visibility = 'visible';
 })
